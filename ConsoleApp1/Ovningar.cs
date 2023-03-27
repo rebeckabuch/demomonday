7c346ea075eff92be6ee42a33d25fb84689e5ff4
Hule <Hule@WINDOWS-LLGIAG4>
2023-03-27 20:13:11 +0200

    Add project files.

---
+++ ConsoleApp1/Ovningar.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleApp1
+//{
+//    public class Ovningar
+//    {
+//        static void GetAnvandarData()
+//        {
+//            Console.WriteLine("ange din epost");
+//            string email = Console.ReadLine();
+
+//            Console.WriteLine("skriv ditt förnamn");
+//            string fornamn = Console.ReadLine();
+//        }
+
+
+
+//    }
+//}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Ovningar
+    {
+        static void Main() {
+            GetAnvandarData();
+        }
+        static void GetAnvandarData()
+        {
+            Console.WriteLine("ange din epost");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("skriv ditt förnamn");
+            string fornamn = Console.ReadLine();
+        }
+
+
+
+    }
+}
