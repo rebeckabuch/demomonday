934aead82ef9bccff756aa487c875c92dc7683d5
Rebecka Buch <Rebeckabuch@hotmail.com>
2023-03-28 15:40:43 +0200

    La till övningsuppgifter som kommentarer

---
+++ ConsoleApp1/Ovningsuppgifter.cs
@@ -0,0 +1,39 @@
+﻿//Programming Förberedande kurs
+//Jag vill att du ska skapa ett enklare program med nedanstående funktioner. Förslagsvis i ett 
+//konsolprojekt i Visual Studio. 
+//Programmet ska bestå av ett menysystem där du med hjälp av inmatning ska kunna välja vilken 
+//funktionalitet som ska köras. Programmet ska hållas igång tills du väljer alternativet för att stänga ner 
+//det (förslagsvis med valet 0 i menyn). 
+//Jag vill ha dessa funktioner:
+//1.Funktion som skriver ut ”Hello World” i konsolen
+//2. Funktion som tar in input från användaren (Förnamn, Efternamn, Ålder) och sedan skriver ut 
+//dessa i konsolen
+//3. Funktion som ändrar färgen på texten i konsolen (och ändrar tillbaka om man använder 
+//funktionen igen
+//4. Funktion för att skriva ut dagens datum
+//5. Funktion som tar två input värden, sedan skriver ut vilket av dem som är störst.
+//6. Funktion som genererar att slumpmässigt tal mellan 1 och 100. Användaren ska sedan gissa 
+//talet. Gissar användaren rätt så ska ett meddelande säga detta, samt hur många försök det 
+//tog. Gissar användaren fel ska ett meddelande visas som informerar ifall talet var för stort 
+//eller för litet
+//7. Funktion där användaren skriver in en textrad, som sedan sparas i en fil på hårddisken
+//8. Funktion där en fil läses in från hårddisken (för enkelhetens skull kan man använda filen från 
+//uppgift 7)
+//9.Funktion där användaren skickar in ett decimaltal och får tillbakaroten ur, upphöjt till 2 och 
+//upphöjt till 10
+//10. Funktion där programmet skriver ut en multiplikationstabell från 1 till 10. En ”tabb” ska 
+//läggas in efter varje nummer. Försöka att ställa upp det så det blir relativt läsbart.
+//11. Funktion som skapar två arrayer. Den första fylls med slumpmässiga tal. Den andra fylls med 
+//talen från den första i stigande ordning. Array.Sort() får EJ användas.
+//12. Funktion som tar en input från användaren och kontrollerar ifall det är en palindrom (alltså 
+//samma ord från båda håll, såsom Anna eller radar.
+//13. Funktion som tar två inputs från användaren och skriver sedan ut alla siffror som är mellan 
+//de två inputsen.
+//14. Funktion där användaren skickar in ett antal värden (komma-separerade siffror) som sedan 
+//sorteras och skrivs ut efter udda och jämna värden. 
+//15. Funktion där användaren skriver in ett antal värden(komma-separerade siffor) som sedan 
+//adderas och skrivs ut.
+//16. Funktion där användaren ska ange namnet på sin karaktär och namnet på en motståndare. 
+//Funktionen skall sedan själv lägga till slumpmässiga värden för Hälsa, Styrka och Tur, som 
+//sparas i en instans av en klass.
+//Lycka till!
